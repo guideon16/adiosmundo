ecfb385f844f45398688b3b11d53452ba0d75bfb
guideon16 <pablomrocker@gmail.com>
2019-09-26 20:29:01 +0200

    poniendo etiquetas actualizando versiones

--- adiosmundo.java
+++ adiosmundo.java
@@ -2,5 +2,6 @@ public class adiosmundo {
 
 	public static void main(String[] args){
 	system.out.print ("AdiosMundo tontito viva la programacion y soy programador");
+	system,our.print ("pero volvere pimpinelas");
 	}
 }
