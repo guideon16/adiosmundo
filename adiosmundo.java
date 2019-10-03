0c4dcf03840725ebddd1840e3758d2107dfa782c
Camilo Albert <pepico.valencia@gmail.com>
2019-10-03 20:11:13 +0200

    Modificacion del java

--- adiosmundo.java
+++ adiosmundo.java
@@ -1,7 +1,9 @@
 public class adiosmundo {
 
+// modificado por Ryodaw19, Country road.
+
 	public static void main(String[] args){
-	system.out.print ("AdiosMundo tontito viva la programacion y soy programador");
+	system.out.print ("AdiosMundo viva la programacion y el rock,  soy programador asds");
 	system,our.print ("pero volvere pimpinelas");
 	}
 }
